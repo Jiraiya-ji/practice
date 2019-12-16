d4b81450124bc4a6af1f19c10e71a7ed6857cbd2
Your Name <you@example.com>
2019-12-17 00:03:27 +0800

    Merge

--- src/main/java/com/example/testdemo/leetcode/MergeTwoLists.java
+++ src/main/java/com/example/testdemo/leetcode/MergeTwoLists.java
@@ -4,30 +4,10 @@ import com.example.testdemo.ListNode;
 
 public class MergeTwoLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode cur = l2;
-        ListNode k = l1;
-        //问题如何记这个指针
-        while(cur!=null){
-            //之前
-            if(cur.val<k.val){
-                ListNode next = cur.next;
-                cur.next = k;
-                k=cur;//返回值
-                cur = next;
-            }//中间
-            else if(cur.val>=k.val&&cur.val<k.next.val){
-                ListNode next = cur.next;
-                ListNode mid = k.next;
-                k.next = cur;
-                cur.next = mid;
-                cur = next;
-            }//之后
-            else {
-                if(k.next==null){
-                    k.next = cur;
-                }
-                k = k.next;
-            }
-        }
+        //假设 l2 只有一个节点
+        //是否是头节点
+        //是中间节点的话
+        //是否是尾节点
+        return l1;
     }
 }
