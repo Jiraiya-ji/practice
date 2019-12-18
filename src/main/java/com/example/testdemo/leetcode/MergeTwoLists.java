91fabd74d2c21bfc5d8df8bfa296acfcb3a631af
吉鹏程 <1024460357@qq.com>
2019-12-18 19:32:24 +0800

    实现单个链表合并

--- src/main/java/com/example/testdemo/leetcode/MergeTwoLists.java
+++ src/main/java/com/example/testdemo/leetcode/MergeTwoLists.java
@@ -7,34 +7,23 @@ public class MergeTwoLists {
         MergeTwoLists mergeTwoLists = new MergeTwoLists();
     }
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        //假设 l2 只有一个节点
-        ListNode k = l1;         //记录指针
-        //是否是头节点
-        ListNode head = l1;
-        ListNode cur = l2;
-        while (cur!=null){
-            if(cur.val<=head.val){
-                cur.next = l1;
-                l1 = cur;
-            }
-            //是中间节点的话
-            else if(cur.val>k.val&&cur.val<k.val){
-                ListNode mid = k.next;
-                k.next = l2;
-                l2.next = mid;
-            }
-            //进行移动，并且判断是否是尾节点
-            else {
-                //尾结点
-                if(k.next.next==null){
-                    k.next = cur;
-                }
-                else{
-                    k = k.next;
-                }
-            }
-            cur=cur.next;
+        if(l1==null){
+            return l2;
         }
-        return l1;
+        if(l2==null){
+            return l1;
+        }
+        ListNode store = null;
+        while (l1.next!=null&&l2.next!=null){
+            if (l1.val>l2.val){
+                store.next = l2;
+                l2 = l2.next;
+            }else {
+                store.next =l1;
+                l1 = l1.next;
+            }
+        }
+        store.next = l1==null?l2:l1;
+        return store;
     }
 }
