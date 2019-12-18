81d0d9d4f78e4b30fd3ac5bc38b6277f687da1f9
Your Name <you@example.com>
2019-12-18 23:10:52 +0800

    树——初识

---
+++ src/main/java/com/example/testdemo/leetcode/MaxDepth.java
@@ -0,0 +1,18 @@
+package com.example.testdemo.leetcode;
+
+import com.fasterxml.jackson.core.TreeNode;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+
+    }
+
+}
+class Treenode{
+    public int val;
+    public Treenode left;
+    public Treenode right;
+    Treenode (int x){
+        this.val = x;
+    };
+}
