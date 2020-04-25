4854e4398e2bcaa01d6cb384bc98c9fa4899eced
jiraiya <1024460357@qq.com>
2020-04-26 00:16:59 +0800

    计算长度最小的数组

--- src/main/java/com/example/testdemo/leetcode/MinSubArrayLen.java
+++ src/main/java/com/example/testdemo/leetcode/MinSubArrayLen.java
@@ -1,10 +1,35 @@
 package com.example.testdemo.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
  */
 public class MinSubArrayLen {
     public int minSubArrayLen(int s, int[] nums) {
-        return 1;
+        if(nums==null||nums.length<0){
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int size = nums.length;
+        while (right<nums.length){
+            if(sum>=s){
+                if(right-left+1<size){
+                    size=right-left+1;
+                }
+                sum-=nums[left];
+                left++;
+            }else {
+               right++;
+               sum+=nums[right];
+            }
+        }
+        return size;
+
+    }
+    public static void main(String[] args){
+
     }
 }
